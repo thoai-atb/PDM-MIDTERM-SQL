57d3ec8c4ba53114ab1706a286d69eae3585939c
thoai-atb <51851596+thoai-atb@users.noreply.github.com>
2020-05-04 14:52:44 +0700

    Add delete cascade constraints

--- create-tables.sql
+++ create-tables.sql
@@ -1,4 +1,5 @@
 USE CollegeDatabase
+
 CREATE TABLE Person.UserInfo(
 	info_id BIGINT IDENTITY(1,1) PRIMARY KEY,
 	login_pass VARCHAR (20) NOT NULL DEFAULT '123456',
@@ -45,23 +46,23 @@ CREATE TABLE Enroll.Enrolled(
 );
 
 ALTER TABLE Person.Staff
-ADD CONSTRAINT FK_Staff_UserInfo FOREIGN KEY (info_id) REFERENCES Person.UserInfo(info_id);
+ADD CONSTRAINT FK_Staff_UserInfo FOREIGN KEY (info_id) REFERENCES Person.UserInfo(info_id) ON DELETE CASCADE;
 
 ALTER TABLE Person.Student
-ADD CONSTRAINT FK_Student_UserInfo FOREIGN KEY (info_id) REFERENCES Person.UserInfo(info_id),
+ADD CONSTRAINT FK_Student_UserInfo FOREIGN KEY (info_id) REFERENCES Person.UserInfo(info_id) ON DELETE CASCADE,
 	CONSTRAINT FK_Student_Department FOREIGN KEY (department_id) REFERENCES College.Department(department_id);
 
 ALTER TABLE Person.Instructor
-ADD CONSTRAINT FK_Instructor_UserInfo FOREIGN KEY (info_id) REFERENCES Person.UserInfo(info_id),
+ADD CONSTRAINT FK_Instructor_UserInfo FOREIGN KEY (info_id) REFERENCES Person.UserInfo(info_id) ON DELETE CASCADE,
 	CONSTRAINT FK_Instructor_Department FOREIGN KEY (department_id) REFERENCES College.Department(department_id);
 
 ALTER TABLE College.Department
 ADD CONSTRAINT FK_Department_HeadInstructor FOREIGN KEY (head_instructor) REFERENCES Person.Instructor(instructor_id);
 
 ALTER TABLE College.Course
-ADD CONSTRAINT FK_Course_Department FOREIGN KEY (department_id) REFERENCES College.Department(department_id),
+ADD CONSTRAINT FK_Course_Department FOREIGN KEY (department_id) REFERENCES College.Department(department_id) ON DELETE CASCADE,
 	CONSTRAINT FK_Course_Instructor FOREIGN KEY (instructor_id) REFERENCES Person.Instructor(instructor_id);
-
+	
 ALTER TABLE Enroll.Enrolled
-ADD CONSTRAINT FK_Enrolled_Student FOREIGN KEY (student_id) REFERENCES Person.Student(student_id),
-	CONSTRAINT FK_Enrolled_Course FOREIGN KEY (course_id) REFERENCES College.Course(course_id);
+ADD CONSTRAINT FK_Enrolled_Student FOREIGN KEY (student_id) REFERENCES Person.Student(student_id) ON DELETE CASCADE,
+	CONSTRAINT FK_Enrolled_Course FOREIGN KEY (course_id) REFERENCES College.Course(course_id) ON DELETE CASCADE;
