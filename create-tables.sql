7bd92861a57494f5dac5efe3f0d2e14ba760e4b3
thoai-atb <51851596+thoai-atb@users.noreply.github.com>
2020-05-08 17:44:43 +0700

    Add files via upload

--- create-tables.sql
+++ create-tables.sql
@@ -57,12 +57,12 @@ ADD CONSTRAINT FK_Instructor_UserInfo FOREIGN KEY (info_id) REFERENCES Person.Us
 	CONSTRAINT FK_Instructor_Department FOREIGN KEY (department_id) REFERENCES College.Department(department_id);
 
 ALTER TABLE College.Department
-ADD CONSTRAINT FK_Department_HeadInstructor FOREIGN KEY (head_instructor) REFERENCES Person.Instructor(instructor_id);
+ADD CONSTRAINT FK_Department_HeadInstructor FOREIGN KEY (head_instructor) REFERENCES Person.Instructor(instructor_id) ON UPDATE CASCADE ON DELETE SET NULL;
 
 ALTER TABLE College.Course
 ADD CONSTRAINT FK_Course_Department FOREIGN KEY (department_id) REFERENCES College.Department(department_id) ON DELETE CASCADE,
-	CONSTRAINT FK_Course_Instructor FOREIGN KEY (instructor_id) REFERENCES Person.Instructor(instructor_id);
+	CONSTRAINT FK_Course_Instructor FOREIGN KEY (instructor_id) REFERENCES Person.Instructor(instructor_id) ON UPDATE CASCADE ON DELETE SET NULL;
 	
 ALTER TABLE Enroll.Enrolled
-ADD CONSTRAINT FK_Enrolled_Student FOREIGN KEY (student_id) REFERENCES Person.Student(student_id) ON DELETE CASCADE,
+ADD CONSTRAINT FK_Enrolled_Student FOREIGN KEY (student_id) REFERENCES Person.Student(student_id) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT FK_Enrolled_Course FOREIGN KEY (course_id) REFERENCES College.Course(course_id) ON DELETE CASCADE;
